bba0a0d38ef89e8753a8a4cb008d3c5513cb23b8
raghurks <raghavendra_s@oyespace.com>
2020-04-09 15:15:08 +0530

    neeeuasdfhe

--- src/app/family-members/family-members.component.ts
+++ src/app/family-members/family-members.component.ts
@@ -53,6 +53,7 @@ export class FamilyMembersComponent implements OnInit {
   ngOnInit() {
     this.memberList=true;
     this.addMember=false;
+    this.Relation="Select Relation"
     this.getFamilyMember();
   }
   ngAfterViewInit() {
@@ -220,7 +221,7 @@ addfamilymember() {
         }).then(
           (result) => {
             if (result.value) {
-              this.Relation='';
+              this.Relation="Select Relation";
               this.FirstName='';
               this.MobileNumber='';
               // this.ToggleGurdian='';           
