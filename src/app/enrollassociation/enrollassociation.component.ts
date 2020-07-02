335a26cebf958e25f44fefbf1e798eab9bbbb6d0
silversatya <Satyanarayana_v@oyespace.com>
2020-07-02 12:57:23 +0530

    Fixed OW-460,OW-450

--- src/app/enrollassociation/enrollassociation.component.ts
+++ src/app/enrollassociation/enrollassociation.component.ts
@@ -578,7 +578,7 @@ imgfilename;
  
   }
   keyPress3(event:any){
-    const pattern = /^[1-9][0-9]*$/;
+     const pattern = /^[1-9][0-9]*$/;
     let inputChar = String.fromCharCode(event.charCode);
     // console.log(inputChar, e.charCode);
        if (!pattern.test(inputChar)) {
